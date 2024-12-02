1fba785941d1e47c5d7a76b2162f0e8195234648
Lashen Martino <lashenmartino@gmail.com>
2024-12-02 07:45:59 +0530

    Implement customer dashboard

--- frontend/src/types/Ticket.ts
+++ frontend/src/types/Ticket.ts
@@ -2,4 +2,13 @@ export interface Ticket {
     id: number;
     available: boolean;
     ticketPrice: number;
-}
+  }
+  
+  export interface TicketDTO {
+    eventId: any;
+    eventName: string;
+    ticketId: string;
+    imageUrl: string;
+    dateTime: string;
+  }
+  
